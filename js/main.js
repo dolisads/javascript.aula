d1f9d96679bb48417ba4e5e6512bb1d8e96dc539
dolisads <profissionalbasico@gmail.com>
2021-04-01 13:08:39 -0300

    testendo script

---
+++ js/main.js
@@ -0,0 +1,136 @@
+//teste java
+
+et valor1 = parseInt(gets(this.valor1));
+let valor2 = parseInt(gets(this.valor2));
+let valor3 = parseFloat(gets(this.valor3));
+let salary = parseFloat(1 * 1).toFixed(2); // Digite aqui o calculo do salário
+console.log("NUMBER = " + valor1);
+console.log("SALARY = U$ " + salary);
+
+
+
+
+
+
+//manipulando elementos da pagina//
+
+/*function clicou(){
+    document.getElementById("agradecimento").innerHTML ="Obrigado por clicar"
+    //alert("Obrigado por clicar");
+}
+function redirecionar(){
+    window.open("https://www.google.com");
+}
+
+function trocar(elemento){
+   // document.getElementById("mousemove").innerHTML ="Obrigado por passar o mouse"
+elemento.innerHTML ="Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+function voltar(elemento){
+   // document.getElementById("mousemove").innerHTML ="passe o mouse aqui"
+   elemento.innerHTML ="passe o mouse aqui";
+
+}
+
+function load(){
+    alert("página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+
+}/*
+
+
+/*function soma (n1 ,n2 ) {
+    return n1 + n2;
+}*/
+
+
+
+//desenvolvendo pagina web//
+/*function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        var validar = true;
+
+    } else { 
+        validar = false;
+    }
+    return validar;
+}
+    var idade = prompt("Qual sua idade?");
+    console.log(validaIdade(idade));*/
+
+//var d = new Date();
+//alert(d.getDay());
+//alert(d.getHours());
+//alert(d.getMinutes());
+
+
+
+
+/*var  count;
+for (count=0; count <= 5; count++) {
+    alert(count);
+};*/
+
+
+ /*var count = 0;
+ while (count < 5);{
+     console.log(count)
+     alert(count);
+     count++;
+ };*/
+ 
+ // Laços de repetição
+
+/*var idade = prompt("Qual sua idade?");
+
+ if (idade >= 18) { 
+     alert("maior de idade");
+    } else {
+        alert ("menor de idade");
+
+    };/*
+ 
+ 
+ 
+ //dicionário
+/*
+ var frutas = [{nome:"abacate", cor:"verde"}, {nome:"morango", cor:"vermelha"} ] //jason
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+
+//var fruta = {nome:"abacate", cor:"verde"} //jason
+//console.log(fruta.cor);
+//alert(fruta.nome);
+
+
+//Array
+
+/*var lista = ["maça", "pêra", "laranja"];
+lista.push("uva"); //acrescentar intem a lista
+lista.pop(); //retirar ultimo item da lista*/
+
+
+//console.log(lista);
+//alert(lista[1]);
+//console.log(lista.reverse());
+//console.log(lista.toString()); //transforma em string
+//console.log(lista.join( " / " ));// separas as strings
+
+             //váriaveis
+//var nome =" Dolisdeis Veloso";
+//var idade =  29;
+//var idade2 = 39;
+//var frase= "Japão é o melhor time do mundo";
+//alert (nome + " tem " + idade + " anos ");
+//alert (" Bem vindo" + nome + " tem" + idade  + " anos");
+//console.log (nome);
+//console.log(idade + nome)
+//console.log(frase.toLowerCase());
+//alert (frase.replace("Japão, "Brasil"));
+//alert (idade + iadade2);
